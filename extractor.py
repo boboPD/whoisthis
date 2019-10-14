4eef1ea21efab52199bf0ce1445b3cbdda30a240
Pradyumna Das <pd10@illinois.edu>
2019-10-15 01:29:38 +0530

    working with venv

--- extractor.py
+++ extractor.py
@@ -6,15 +6,12 @@ import os
 
 if len(sys.argv) != 2:
     print("please provide the path to the epub contents")
-    
-#Changing the working directory to the folder containing the epub contents so that relative pathing works easily
-os.chdir(sys.argv[1])
 
 spine_xml = ""
 toc_xml = ""
-with open("content.opf", "r", encoding="utf8") as spine_file:
+with open(f"{sys.argv[1]}/content.opf", "r", encoding="utf8") as spine_file:
     spine_xml = spine_file.read()
-with open("toc.ncx", "r", encoding="utf8") as toc_file:
+with open(f"{sys.argv[1]}/toc.ncx", "r", encoding="utf8") as toc_file:
     toc_xml = toc_file.read()
     
 spine_contents = BeautifulSoup(spine_xml, "lxml-xml")
@@ -26,7 +23,7 @@ print(f"Found {len(html_page_list)} html files in epub")
 book_contents = ""
 for content_file in html_page_list:
     print(f"Processing file: {content_file}")
-    with open(content_file, "r", encoding="utf8") as filereader:
+    with open(f"{sys.argv[1]}/{content_file}", "r", encoding="utf8") as filereader:
         soup = BeautifulSoup(filereader.read(), "lxml")
         body = soup.find("body")
         
@@ -34,5 +31,7 @@ for content_file in html_page_list:
             book_contents += item
 
 print("Writing complete contents to output file")
-with open("output.txt", "w", encoding="utf8") as output:
+
+os.makedirs("./output")
+with open("./output/book.txt", "w", encoding="utf8") as output:
     output.write(book_contents)
